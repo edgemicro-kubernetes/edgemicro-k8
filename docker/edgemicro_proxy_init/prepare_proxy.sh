cc668cd7a12ad1526523d7356ce21648e23ca97a
Rajesh Mishra <rajeshmi@google.com>
2018-03-16 01:09:53 -0700

    init proxy outbut redirection bypassed

--- docker/edgemicro_proxy_init/prepare_proxy.sh
+++ docker/edgemicro_proxy_init/prepare_proxy.sh
@@ -53,33 +53,33 @@ fi
 
 # Create a new chain for redirecting inbound and outbound traffic to
 # the common Envoy port.
-iptables -t nat -N ISTIO_REDIRECT                                             -m comment --comment "istio/redirect-common-chain"
-iptables -t nat -A ISTIO_REDIRECT -p tcp -j REDIRECT --to-port ${ENVOY_PORT}  -m comment --comment "istio/redirect-to-envoy-port"
+iptables -t nat -N EDGEMICRO_REDIRECT                                             -m comment --comment "edgemicro/redirect-common-chain"
+iptables -t nat -A EDGEMICRO_REDIRECT -p tcp -j REDIRECT --to-port ${ENVOY_PORT}  -m comment --comment "edgemicro/redirect-to-envoy-port"
 
 # Redirect all inbound traffic to Envoy.
-iptables -t nat -A PREROUTING -j ISTIO_REDIRECT                               -m comment --comment "istio/install-istio-prerouting"
+iptables -t nat -A PREROUTING -j EDGEMICRO_REDIRECT                               -m comment --comment "edgemicro/install-edgemicro-prerouting"
 
 # Create a new chain for selectively redirecting outbound packets to
 # Envoy.
-iptables -t nat -N ISTIO_OUTPUT                                               -m comment --comment "istio/common-output-chain"
+iptables -t nat -N EDGEMICRO_OUTPUT                                               -m comment --comment "edgemicro/common-output-chain"
 
-# Jump to the ISTIO_OUTPUT chain from OUTPUT chain for all tcp
-# traffic. '-j RETURN' bypasses Envoy and '-j ISTIO_REDIRECT'
+# Jump to the EDGEMICRO_OUTPUT chain from OUTPUT chain for all tcp
+# traffic. '-j RETURN' bypasses Envoy and '-j EDGEMICRO_REDIRECT'
 # redirects to Envoy.
-iptables -t nat -A OUTPUT -p tcp -j ISTIO_OUTPUT                              -m comment --comment "istio/install-istio-output"
+iptables -t nat -A OUTPUT -p tcp -j EDGEMICRO_OUTPUT                              -m comment --comment "edgemicro/install-edgemicro-output"
 
 # Redirect app calls to back itself via Envoy when using the service VIP or endpoint
 # address, e.g. appN => Envoy (client) => Envoy (server) => appN.
-iptables -t nat -A ISTIO_OUTPUT -o lo ! -d 127.0.0.1/32 -j ISTIO_REDIRECT     -m comment --comment "istio/redirect-implicit-loopback"
+iptables -t nat -A EDGEMICRO_OUTPUT -o lo ! -d 127.0.0.1/32 -j EDGEMICRO_REDIRECT     -m comment --comment "edgemicro/redirect-implicit-loopback"
 
 # Avoid infinite loops. Don't redirect Envoy traffic directly back to
 # Envoy for non-loopback traffic.
-iptables -t nat -A ISTIO_OUTPUT -m owner --uid-owner ${ENVOY_UID} -j RETURN   -m comment --comment "istio/bypass-envoy"
+iptables -t nat -A EDGEMICRO_OUTPUT -m owner --uid-owner ${ENVOY_UID} -j RETURN   -m comment --comment "edgemicro/bypass-envoy"
 
 # Skip redirection for Envoy-aware applications and
 # container-to-container traffic both of which explicitly use
 # localhost.
-iptables -t nat -A ISTIO_OUTPUT -d 127.0.0.1/32 -j RETURN                     -m comment --comment "istio/bypass-explicit-loopback"
+iptables -t nat -A EDGEMICRO_OUTPUT -d 127.0.0.1/32 -j RETURN                     -m comment --comment "edgemicro/bypass-explicit-loopback"
 
 # All outbound traffic will be redirected to Envoy by default. If
 # IP_RANGES_INCLUDE is non-empty, only traffic bound for the
@@ -87,11 +87,12 @@ iptables -t nat -A ISTIO_OUTPUT -d 127.0.0.1/32 -j RETURN                     -m
 IFS=,
 if [ "${IP_RANGES_INCLUDE}" != "" ]; then
     for cidr in ${IP_RANGES_INCLUDE}; do
-        iptables -t nat -A ISTIO_OUTPUT -d ${cidr} -j ISTIO_REDIRECT          -m comment --comment "istio/redirect-ip-range-${cidr}"
+        iptables -t nat -A EDGEMICRO_OUTPUT -d ${cidr} -j EDGEMICRO_REDIRECT          -m comment --comment "edgemicro/redirect-ip-range-${cidr}"
     done
-    iptables -t nat -A ISTIO_OUTPUT -j RETURN                                 -m comment --comment "istio/bypass-default-outbound"
+    iptables -t nat -A EDGEMICRO_OUTPUT -j RETURN                                 -m comment --comment "edgemicro/bypass-default-outbound"
 else
-    iptables -t nat -A ISTIO_OUTPUT -j ISTIO_REDIRECT                         -m comment --comment "istio/redirect-default-outbound"
+    iptables -t nat -A EDGEMICRO_OUTPUT -j RETURN                                 -m comment --comment "edgemicro/bypass-default-outbound"
 fi
+
 
 exit 0
