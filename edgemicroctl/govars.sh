d03d58dacb86be35d30754c8afea4600406328de
Rajesh Mishra <rajeshmi@google.com>
2018-08-05 18:02:09 -0700

    govars

--- edgemicroctl/govars.sh
+++ edgemicroctl/govars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GOPATH=/Users/rajeshmi/presales/git/edgemicro-k8/edgemicroctl
-export GOROOT=/usr/lib/google-golang
+#export GOROOT=/usr/lib/google-golang
 export PATH=$PATH:/Users/rajeshmi/presales/git/edgemicro-k8/edgemicroctl/bin
